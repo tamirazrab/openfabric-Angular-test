5194cb6accd202ea52b1806c9395e6c60d53ac15
T.A <tamirazrab@gmail.com>
2023-05-26 15:06:37 +0500

    feat: :sparkles: add user model controller routes

---
+++ apps/api/src/modules/user/user.interfaces.ts
@@ -0,0 +1,27 @@
+import mongoose, { Model, Document } from 'mongoose';
+import { QueryResult } from '../paginate/paginate';
+import { AccessAndRefreshTokens } from '../token/token.interfaces';
+
+export interface IUser {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface IUserDoc extends IUser, Document {
+  isPasswordMatch(password: string): Promise<boolean>;
+}
+
+export interface IUserModel extends Model<IUserDoc> {
+  isEmailTaken(email: string, excludeUserId?: mongoose.Types.ObjectId): Promise<boolean>;
+  paginate(filter: Record<string, any>, options: Record<string, any>): Promise<QueryResult>;
+}
+
+export type UpdateUserBody = Partial<IUser>;
+
+export type NewRegisteredUser = Omit<IUser, 'isEmailVerified'>;
+
+export interface IUserWithTokens {
+  user: IUserDoc;
+  tokens: AccessAndRefreshTokens;
+}
