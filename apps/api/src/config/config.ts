7d662035d75095c6e23d9c5267f8dd6cd7eb8287
T.A <tamirazrab@gmail.com>
2023-05-29 11:52:52 +0500

    chore: :lipstick: change site title

--- apps/api/src/config/config.ts
+++ apps/api/src/config/config.ts
@@ -9,9 +9,6 @@ const envVarsSchema = Joi.object()
     JWT_SECRET: Joi.string().required().description('JWT secret key'),
     JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
     JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
-    JWT_RESET_PASSWORD_EXPIRATION_MINUTES: Joi.number()
-      .default(10)
-      .description('minutes after which reset password token expires'),
     CLIENT_URL: Joi.string().required().description('Client url'),
   })
   .unknown();
@@ -37,7 +34,6 @@ const config = {
     secret: envVars.JWT_SECRET,
     accessExpirationMinutes: envVars.JWT_ACCESS_EXPIRATION_MINUTES,
     refreshExpirationDays: envVars.JWT_REFRESH_EXPIRATION_DAYS,
-    resetPasswordExpirationMinutes: envVars.JWT_RESET_PASSWORD_EXPIRATION_MINUTES,
     cookieOptions: {
       httpOnly: true,
       secure: envVars.NODE_ENV === 'production',
